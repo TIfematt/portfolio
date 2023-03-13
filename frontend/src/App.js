7a17ed0e0806e1a5bf07aaa8040069cc215e5b11
Boluwatife <boluwatifeoladejo@gmail.com>
2023-03-13 13:44:15 +0100

    Removed preloader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import {About, Footer, Header, Skills, Testimonial, Work} from './containers'
-import { Navbar, Preloader } from './components'
+import { Navbar} from './components'
 import './App.scss';
 
 const App = () => {
   return (
     <div className='app'>
-      <Preloader />
       <Navbar />
       <Header />
       <About />
